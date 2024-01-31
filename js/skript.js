f6bedad477a98d1e67f2863d0140e3548b91fe8b
Insider <your_email@whatever.com>
2024-01-31 18:07:51 +0200

    fourthExerciseExampleTwo

--- js/skript.js
+++ js/skript.js
@@ -51,14 +51,25 @@ let personalMovieDB = {
         }
     },
     writeYourGenres: function() {
-        for (let i = 1; i <= 3; i++) {
-            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        for (let i = 1; i <= 2; i++) {
+            // let genre = prompt(`Ваш любимый жанр под номером ${i}`);
 
-            if (genre === '' || genre === null) {
+            // if (genre === '' || genre === null) {
+            //     console.log('Вы ввели некорректные данные или не ввели их вообще');
+            //     i--;
+            // } else {
+            //     personalMovieDB.genres[i - 1] = genre;
+            // }
+
+
+            let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase;
+
+            if (genres === '' || genres === null) {
                 console.log('Вы ввели некорректные данные или не ввели их вообще');
                 i--;
             } else {
-                personalMovieDB.genres[i - 1] = genre;
+                personalMovieDB.genres = genres.split(', ');
+                personalMovieDB.genres.sort();
             }
         }
 
